a6aecdd3c2f4aa60ec08aac91274f2bb8f695055
xieydd <xieydd@gmail.com>
2018-06-26 20:29:43 +0800

    self validate https client

---
+++ tls/self-https-client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	c := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{RootCAs: loadCA("server.crt")},
+		},
+	}
+	if resp, e := c.Get("https://localhost:70"); e != nil {
+		log.Fatal("http.Client.Get", e)
+	} else {
+		defer resp.Body.Close()
+		io.Copy(os.Stdout, resp.Body)
+	}
+}
+
+func loadCA(caFile string) *x509.CertPool {
+	pool := x509.NewCertPool()
+
+	if ca, e := ioutil.ReadFile(caFile); e != nil {
+		log.Fatal("ReadFile: ", e)
+	} else {
+		pool.AppendCertsFromPEM(ca)
+	}
+	return pool
+
+}
